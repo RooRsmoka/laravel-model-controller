6653d67ec2d6bb57b2393c230392332854f32dcd
RooRsmoka <chri.disa96@gmail.com>
2022-03-06 23:04:44 +0100

    created page cotrollers

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
-{
-    //
+class PageController extends Controller {
+
+    public function index() {
+        return view('home');
+    }
 }
